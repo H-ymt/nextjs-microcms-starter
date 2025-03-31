64b1e5f33aefda4ede8c15d8b581a2badf5b3be6
半代大和 <y.handai1272@gmail.com>
2025-03-31 22:59:20 +0900

    feat: Update search functionality to use async/await for searchParams

--- app/blog/search/page.tsx
+++ app/blog/search/page.tsx
@@ -4,15 +4,13 @@ import { BLOG_LIST_LIMIT } from "@/app/constants";
 import { getBlogList } from "@/app/libs/microcms";
 
 type Props = {
-  searchParams: {
-    q?: string;
-  };
+  searchParams: Promise<{ q?: string }>;
 };
 
 export default async function Paga({ searchParams }: Props) {
   const { contents: blog } = await getBlogList({
     limit: BLOG_LIST_LIMIT,
-    q: searchParams.q,
+    q: (await searchParams).q,
   });
 
   return (
